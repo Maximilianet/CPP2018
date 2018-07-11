e98688dd7d5a7ccc93add4aeeb049ed4d66d1d99
im <im@im.im>
2018-07-11 14:03:16 +0300

    doxygen instructions

--- power.cpp
+++ power.cpp
@@ -1,13 +1,24 @@
+/*! \file */ 
 #include <iostream>
+//! \fn Raise int to specific power
+/*!
+ \param base Integer to raise in specific power
+ \param power Power
+ \return result of operation
+*/
+int raisePower(int base, int power)
+{
+ int result=1;
+ for (int i=0; i<=power; i++)
+   result*=base;
+ return result;
+}
 int main()
 {
-int b=2;
+int b=2,p;
 for(int i=0;i<10;++i)
 {
-int p;
-p=1;
-for(int t=1;t<=i;++t)
-p=p*b;
+p=raisePower(b,i);
 std::cout<<p<<" ";
 }
 std::cout<<std::endl;
